33c6150f34686d7c0c1bba9d3b189460e276978f
Joel Guzman <jolman009@yahoo.com>
2024-09-27 18:22:35 -0500

    edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,1 @@
-<script lang="ts">
-	import { onDestroy, onMount } from "svelte"
-
-    onMount(() => console.log('Mounted home page'))
-    onDestroy(() => console.log('Unmounted home page'))
-</script>
-
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/posts">Posts</a>
-        </li>
-    </ul>
-</nav>
-
 <h1>Home page</h1>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,19 +1,3 @@
-<script lang="ts">
-	import { onDestroy, onMount } from "svelte"
-
-    onMount(() => console.log('Mounted posts page'))
-    onDestroy(() => console.log('Unmounted posts page'))
-</script>
-
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/posts">Posts</a>
-        </li>
-    </ul>
-</nav>
-
 <h1>Posts page</h1>
+
+
