096fd6fc630e0447d7499bc76365309a946616cd
Joel Guzman <jolman009@yahoo.com>
2024-09-28 11:26:48 -0500

    edit

---
+++ src/routes/posts/+layout.svelte
@@ -0,0 +1,40 @@
+<div class="posts">
+    <aside>
+        <h4>Posts</h4>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="/posts/one">One</a>
+                </li>
+                <li>
+                    <a href="/posts/two">Two</a>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+
+<main>
+    <slot />
+  </main>
+</div>
+
+<style>
+    .posts {
+        display: grid;
+        gap: 2rem;
+        grid-template-columns: 200px 60ch;
+        margin-top: 2rem;
+        border: 1px solid white;
+    }
+
+    a {
+        border: 1px solid white;
+    }
+
+    
+    h4 {
+        border: 1px solid white;
+        }
+    
+</style>
---
+++ src/routes/posts/create/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+  </script>
+
+<h1>Yikes! 💥</h1>
+
+    <h1>{$page.error?.message}</h1>
+
+    <p>Something went horribly wrong but the blast radius has been contained to this route.</p>
+
+    <pre>
+        {JSON. stringify($page, null, 2)}
+    </pre>
+
